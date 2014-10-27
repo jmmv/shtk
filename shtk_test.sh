2a2d12a6358f02c5ebbb61929e8579166e63f1a2
Julio Merino <jmmv@google.com>
2014-10-27 10:29:16 -0400

    Convert shtk_test to unittest

--- shtk_test.sh
+++ shtk_test.sh
@@ -26,9 +26,11 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+shtk_import unittest
 
-atf_test_case build__defaults
-build__defaults_body() {
+
+shtk_unittest_add_test build__defaults
+build__defaults_test() {
     cat >script.sh <<EOF
 shtk_import cli
 
@@ -37,17 +39,17 @@ main() {
 }
 EOF
 
-    atf_check shtk build script.sh
+    expect_command shtk build script.sh
 
     cat >experr <<EOF
 script: I: It works
 EOF
-    atf_check -e file:experr ./script
+    expect_command -e file:experr ./script
 }
 
 
-atf_test_case build__mflag__explicit
-build__mflag__explicit_body() {
+shtk_unittest_add_test build__mflag__explicit
+build__mflag__explicit_test() {
     cat >script.sh <<EOF
 shtk_import cli
 
@@ -60,17 +62,17 @@ main() {
 }
 EOF
 
-    atf_check shtk build -m my_main script.sh
+    expect_command shtk build -m my_main script.sh
 
     cat >experr <<EOF
 script: I: Print this
 EOF
-    atf_check -e file:experr ./script
+    expect_command -e file:experr ./script
 }
 
 
-atf_test_case build__mflag__empty
-build__mflag__empty_body() {
+shtk_unittest_add_test build__mflag__empty
+build__mflag__empty_test() {
     cat >script.sh <<EOF
 shtk_import cli
 
@@ -81,17 +83,17 @@ main() {
 }
 EOF
 
-    atf_check shtk build -m '' script.sh
+    expect_command shtk build -m '' script.sh
 
     cat >experr <<EOF
 script: I: Outside of main
 EOF
-    atf_check -e file:experr ./script
+    expect_command -e file:experr ./script
 }
 
 
-atf_test_case build__oflag__explicit
-build__oflag__explicit_body() {
+shtk_unittest_add_test build__oflag__explicit
+build__oflag__explicit_test() {
     cat >script.sh <<EOF
 shtk_import cli
 
@@ -99,13 +101,13 @@ main() {
     shtk_cli_info "A string"
 }
 EOF
-    atf_check shtk build -o first script.sh
-    atf_check -e inline:'first: I: A string\n' ./first
+    expect_command shtk build -o first script.sh
+    expect_command -e inline:'first: I: A string\n' ./first
 }
 
 
-atf_test_case build__oflag__stdin
-build__oflag__stdin_body() {
+shtk_unittest_add_test build__oflag__stdin
+build__oflag__stdin_test() {
     cat >script.sh <<EOF
 shtk_import cli
 
@@ -113,104 +115,85 @@ main() {
     shtk_cli_info "A string"
 }
 EOF
-    cat script.sh | atf_check shtk build -o second -
-    atf_check -e inline:'second: I: A string\n' ./second
+    cat script.sh | expect_command shtk build -o second -
+    expect_command -e inline:'second: I: A string\n' ./second
 }
 
 
-atf_test_case build__oflag__necessary
-build__oflag__necessary_body() {
+shtk_unittest_add_test build__oflag__necessary
+build__oflag__necessary_test() {
     touch script
 
     cat >experr <<EOF
 shtk: E: Input file should end in .sh or you must specify -o
 Type 'man shtk' for help
 EOF
-    atf_check -s exit:1 -e file:experr shtk build script
-    echo "foo" | atf_check -s exit:1 -e file:experr shtk build -
+    expect_command -s exit:1 -e file:experr shtk build script
+    echo "foo" | expect_command -s exit:1 -e file:experr shtk build -
 }
 
 
-atf_test_case build__sflag
-build__sflag_body() {
+shtk_unittest_add_test build__sflag
+build__sflag_test() {
     cat >script.sh <<EOF
 We won't run this anyway.
 EOF
-    atf_check shtk build -s '/custom/interpreter' script.sh
-    atf_check -o inline:'#! /custom/interpreter\n' head -n1 script
+    expect_command shtk build -s '/custom/interpreter' script.sh
+    expect_command -o inline:'#! /custom/interpreter\n' head -n1 script
 }
 
 
-atf_test_case build__no_args
-build__no_args_body() {
+shtk_unittest_add_test build__no_args
+build__no_args_test() {
     cat >experr <<EOF
 shtk: E: build takes one argument only
 Type 'man shtk' for help
 EOF
-    atf_check -s exit:1 -e file:experr shtk build
+    expect_command -s exit:1 -e file:experr shtk build
 }
 
 
-atf_test_case build__too_many_args
-build__too_many_args_body() {
+shtk_unittest_add_test build__too_many_args
+build__too_many_args_test() {
     cat >experr <<EOF
 shtk: E: build takes one argument only
 Type 'man shtk' for help
 EOF
-    atf_check -s exit:1 -e file:experr shtk build foo bar
+    expect_command -s exit:1 -e file:experr shtk build foo bar
 }
 
 
-atf_test_case version__ok
-version__ok_body() {
-    atf_check -s exit:0 -o match:"shtk [0-9]+\.[0-9]+.*" shtk version
+shtk_unittest_add_test version__ok
+version__ok_test() {
+    expect_command -s exit:0 -o match:"shtk [0-9]+\.[0-9]+.*" shtk version
 }
 
 
-atf_test_case version__too_many_args
-version__too_many_args_body() {
+shtk_unittest_add_test version__too_many_args
+version__too_many_args_test() {
     cat >experr <<EOF
 shtk: E: version does not take any arguments
 Type 'man shtk' for help
 EOF
-    atf_check -s exit:1 -e file:experr shtk version foo
+    expect_command -s exit:1 -e file:experr shtk version foo
 }
 
 
-atf_test_case no_command
-no_command_body() {
+shtk_unittest_add_test no_command
+no_command_test() {
     cat >experr <<EOF
 shtk: E: No command specified
 Type 'man shtk' for help
 EOF
-    atf_check -s exit:1 -e file:experr shtk
+    expect_command -s exit:1 -e file:experr shtk
 }
 
 
-atf_test_case unknown_command
-unknown_command_body() {
+shtk_unittest_add_test unknown_command
+unknown_command_test() {
     cat >experr <<EOF
 shtk: E: Unknown command foo
 Type 'man shtk' for help
 EOF
-    atf_check -s exit:1 -e file:experr shtk foo
-}
-
-
-atf_init_test_cases() {
-    atf_add_test_case build__defaults
-    atf_add_test_case build__mflag__explicit
-    atf_add_test_case build__mflag__empty
-    atf_add_test_case build__oflag__explicit
-    atf_add_test_case build__oflag__stdin
-    atf_add_test_case build__oflag__necessary
-    atf_add_test_case build__sflag
-    atf_add_test_case build__no_args
-    atf_add_test_case build__too_many_args
-
-    atf_add_test_case version__ok
-    atf_add_test_case version__too_many_args
-
-    atf_add_test_case no_command
-    atf_add_test_case unknown_command
+    expect_command -s exit:1 -e file:experr shtk foo
 }

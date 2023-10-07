67e11162c0118da4c8161e50b2cd8a48d9437948
Julio Merino <julio@meroh.net>
2023-10-07 09:51:16 -0700

    Add a website for shtk
    This creates a minimal website for shtk with the main goal of hosting
    the reference manual.  Otherwise it's impossible for potential users
    to see all of the features included in this library.
    
    This is intentionally simple.  I'm choosing to just create my own
    bespoke templating system to generate the pages given that most of
    the work here is integrating with mandoc.

---
+++ site/build.sh
@@ -0,0 +1,117 @@
+# Copyright 2023 Julio Merino
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+# * Neither the name of Google Inc. nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+shtk_import cli
+
+set -eu
+
+build_distdocs() {
+    local outdir="${1}"; shift
+
+    pandoc INSTALL.md >"${outdir}/install.html.frag"
+}
+
+build_docs() {
+    local outdir="${1}"; shift
+
+    shtk_cli_info "Converting mandoc to HTML..."
+    echo "<ul>" >>"${outdir}/docs.html.frag"
+    for src in man/*.[0-9]; do
+        dest="${src#man/}.html"
+        mandoc -Thtml -Ofragment -Oman=%N.%S.html "${src}" \
+            >"${outdir}/${dest}.frag"
+        echo "<li><a href=\"${dest}\">${dest}</a></li>" \
+            >>"${outdir}/docs.html.frag"
+    done
+    echo "</ul>" >>"${outdir}/docs.html.frag"
+}
+
+build_fragments() {
+    local outdir="${1}"; shift
+    local version="${1}"; shift
+
+    shtk_cli_info "Processing fragments..."
+    for src in "${outdir}"/*.frag; do
+        dest="${src%.frag}"
+
+        docs_active=
+        install_active=
+        case "${src}" in
+            */index.html*) ;;
+            */install.html*) install_active="active" ;;
+            *) docs_active="active" ;;
+        esac
+        cat site/header.html.in "${src}" site/footer.html.in \
+            | sed -e "s,@DOCS_ACTIVE@,${docs_active},g" \
+                  -e "s,@INSTALL_ACTIVE@,${install_active},g" >"${dest}" \
+                  -e "s,@SHTK_VERSION@,${version},g" >"${dest}"
+    done
+}
+
+main() {
+    local outdir=site-out
+    local version=0.0
+
+    while getopts ':o:v:' arg "${@}"; do
+        case "${arg}" in
+            o)  # Output directory.
+                outdir="${OPTARG}"
+                ;;
+
+            v)  # Version to embed in the documentation.
+                version="${OPTARG}"
+                ;;
+
+            :)
+                shtk_cli_error "Missing argument to option -${OPTARG} in build"
+                ;;
+
+            \?)
+                shtk_cli_error "Unknown option -${OPTARG} in build"
+                ;;
+        esac
+    done
+    shift $((${OPTIND} - 1))
+
+    [ ${#} -eq 0 ] || shtk_cli_error "build takes zero arguments"
+
+    rm -rf "${outdir}"
+    mkdir "${outdir}"
+
+    cp site/CNAME "${outdir}"/
+    cp site/*.frag "${outdir}"/
+    cp site/styles.css "${outdir}"/
+
+    build_distdocs "${outdir}"
+
+    build_docs "${outdir}"
+
+    build_fragments "${outdir}" "${version}"
+
+    rm "${outdir}"/*.frag
+}
